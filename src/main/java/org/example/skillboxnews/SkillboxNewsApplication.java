09591398d261860a37bbdf3465faf9630d0cf5de
TeslA1402 <ivantimeev95@gmail.com>
2024-04-02 23:39:04 +0500

    Init commit

---
+++ src/main/java/org/example/skillboxnews/SkillboxNewsApplication.java
@@ -0,0 +1,13 @@
+package org.example.skillboxnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillboxNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillboxNewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/skillboxnews/SkillboxNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.skillboxnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillboxNewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
